a7c6fb1f2c8cb39c4c2de3486290fda8edcb3ca0
이상현 <dkrdj1000@naver.com>
2023-03-24 15:39:29 +0900

    1

---
+++ mrjob.py
@@ -0,0 +1,23 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import socket
+
+class MostPopularMovie(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        song = line.split('/')
+        yield song[0], song[1]
+
+    def reducer_count_ratings(self, key, values):
+        line = key+'/'+values
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        yield line+'\t'+"ip :", ip_address
+
+if __name__ == '__main__':
+    MostPopularMovie.run()
