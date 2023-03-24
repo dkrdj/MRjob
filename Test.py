4adce1a8eb446f76053c8cd90f1f7edaf9623b33
이상현 <dkrdj1000@naver.com>
2023-03-24 15:56:56 +0900

    1

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import socket
+import subprocess
 
 class Test(MRJob):
     def steps(self):
@@ -14,10 +15,25 @@ class Test(MRJob):
         yield song[0], song[1]
 
     def reducer_count_ratings(self, key, values):
-        line = key+'/'+values
+        line = key+'/'+','.join(values)
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         yield line+'\t'+"ip :", ip_address
+
+    def configure_args(self):
+        super(Test, self).configure_args()
+        self.add_file_arg('--input-file', help='path to input file')
+
+    def mapper_init(self):
+        input_file = self.options.input_file
+        if input_file.startswith('hdfs://'):
+            with subprocess.Popen(["hadoop", "fs", "-cat", input_file], stdout=subprocess.PIPE) as proc:
+                self.file = proc.stdout
+        else:
+            self.file = open(input_file, 'r')
+
+    def mapper_final(self):
+        self.file.close()
 
 if __name__ == '__main__':
     Test.run()
